9dc362cb46507110dd45593ef604473f90c8cd3a
giddyan001 <gideonafolayan7@gmail.com>
2026-07-29 02:52:27 +0100

    Add script to find coinbase tx in 07.sh

--- submission/07.sh
+++ submission/07.sh
@@ -1,1 +1,8 @@
 # what is the coinbase tx in this block 243,834
+#!/bin/bash
+
+# Get the block hash for height 243834 and extract the first transaction ID (coinbase)
+block_hash=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 getblockhash 243834)
+
+bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 getblock "$block_hash" | \
+  jq -r '.tx[0]'
