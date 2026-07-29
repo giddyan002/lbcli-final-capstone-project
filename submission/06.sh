a2a26f2420ade6b1e7affd78eff82e40c37e16cb
giddyan001 <gideonafolayan7@gmail.com>
2026-07-29 02:50:46 +0100

    Add script to find RBF signaling transaction in 06.sh

--- submission/06.sh
+++ submission/06.sh
@@ -1,1 +1,8 @@
 # Only one tx in block 243,821 signals opt-in RBF. What is its txid?
+#!/bin/bash
+
+# Get block hash for height 243821, fetch the block with transactions, and find the tx with sequence < 0xfffffffe
+block_hash=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 getblockhash 243821)
+
+bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 getblock "$block_hash" 2 | \
+  jq -r '.tx[] | select(.vin[].sequence < 4294967294) | .txid'
