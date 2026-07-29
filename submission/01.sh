d7684e30744fba9dc1f1ebd7a02b869e1afcb608
giddyan001 <gideonafolayan7@gmail.com>
2026-07-29 02:41:41 +0100

    Add getblockhash script for 01.sh

--- submission/01.sh
+++ submission/01.sh
@@ -1,1 +1,3 @@
 # What is the hash of block 243,833?
+#!/bin/bash
+bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 getblockhash 243833
