0f4bad8cce3366ca7dabbe034dda8beb9a45b256
giddyan001 <gideonafolayan7@gmail.com>
2026-07-29 02:47:40 +0100

    Add script to extract public key for 04.sh

--- submission/04.sh
+++ submission/04.sh
@@ -1,1 +1,6 @@
 # Which public key signed input 0 in this tx: d948454ceab1ad56982b11cf6f7157b91d3c6c5640e05c041cd17db6fff698f7
+#!/bin/bash
+
+# Fetch and decode the transaction, then extract the public key from input 0's witness stack or scriptSig
+bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 getrawtransaction "d948454ceab1ad56982b11cf6f7157b91d3c6c5640e05c041cd17db6fff698f7" 3 | \
+  jq -r '.vin[0].txinwitness[1] // .vin[0].scriptSig.hex'
