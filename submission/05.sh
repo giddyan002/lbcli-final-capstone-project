d52ea100c9319edc48787ce3cdc47026121b5c0d
giddyan001 <gideonafolayan7@gmail.com>
2026-07-29 02:49:27 +0100

    Add script to calculate transaction fee in satoshis for 05.sh

--- submission/05.sh
+++ submission/05.sh
@@ -1,1 +1,6 @@
 # How many satoshis did this transaction pay for fee?: b71fb9ab7707407cc7265591e0c0d47d07afede654f91de1f63c0cb522914bcb
+#!/bin/bash
+
+# Fetch raw transaction with verbosity 3 to directly read the fee field
+bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 getrawtransaction "b71fb9ab7707407cc7265591e0c0d47d07afede654f91de1f63c0cb522914bcb" 3 | \
+  jq '.fee * 100000000 | round'
