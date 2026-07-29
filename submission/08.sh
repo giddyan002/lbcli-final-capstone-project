f7feb214259ff176059d8ac6a1fbf8592d0f860c
giddyan001 <gideonafolayan7@gmail.com>
2026-07-29 02:53:28 +0100

    Add script to find transaction block height in 08.sh

--- submission/08.sh
+++ submission/08.sh
@@ -1,2 +1,8 @@
 # what block height was this tx mined ?
 # 49990a9c8e60c8cba979ece134124695ffb270a98ba39c9824e42c4dc227c7eb
+#!/bin/bash
+
+# Fetch transaction details to get the blockhash where it was included, then fetch block info to get its height
+blockhash=$(bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 getrawtransaction "49990a9c8e60c8cba979ece134124695ffb270a98ba39c9824e42c4dc227c7eb" 1 | jq -r '.blockhash')
+
+bitcoin-cli -signet -rpcuser=btrustbuildersrpc -rpcpassword=btrustbuilderspass -rpcconnect=167.172.185.136 -rpcport=38332 getblock "$blockhash" | jq '.height'
